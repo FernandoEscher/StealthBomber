92fcdb9df3a4c18f2b91f6622e1e83e57f155ab3
Fernando Irías Escher <fernandoiriase@gmail.com>
2013-06-30 04:04:14 -0600

    Primera version jugable.

--- level1.lua
+++ level1.lua
@@ -5,17 +5,30 @@
 -----------------------------------------------------------------------------------------
 
 local storyboard = require( "storyboard" )
+local StickLib   = require("lib_analog_stick")
 local scene = storyboard.newScene()
 
+system.activate( "multitouch" )
+
 -- include Corona's "physics" library
-local physics = require "physics"
-physics.start(); physics.pause()
+local physics = require("physics")
+physics.start()
+physics.setGravity(0, 0)
 
 --------------------------------------------
 
 -- forward declarations and other locals
 local screenW, screenH, halfW = display.contentWidth, display.contentHeight, display.contentWidth*0.5
-local speed = 5
+local backgroundSpeed = 5
+
+screenWidth = display.contentWidth - (display.screenOriginX*2)
+screenHeight = display.contentHeight - (display.screenOriginY*2)
+screenTop = display.screenOriginY
+screenRight = display.contentWidth - display.screenOriginX
+screenBottom = display.contentHeight - display.screenOriginY
+screenLeft = display.screenOriginX
+screenCenterX = display.contentWidth/2
+screenCenterY = display.contentHeight/2
 
 -----------------------------------------------------------------------------------------
 -- BEGINNING OF YOUR IMPLEMENTATION
@@ -29,63 +42,149 @@ local speed = 5
 function scene:createScene( event )
 	local group = self.view
 
-	-- create a grey rectangle as the backdrop
-	-- local background = display.newRect( 0, 0, screenW, screenH )
-	-- background:setFillColor( 128 )
-	
-	-- -- make a crate (off-screen), position it, and rotate slightly
-	-- local crate = display.newImageRect( "crate.png", 90, 90 )
-	-- crate.x, crate.y = 160, -100
-	-- crate.rotation = 15
-	
-	-- -- add physics to the crate
-	-- physics.addBody( crate, { density=1.0, friction=0.3, bounce=0.3 } )
-	
-	-- -- create a grass object and add physics (with custom shape)
-	-- local grass = display.newImageRect( "grass.png", screenW, 82 )
-	-- grass:setReferencePoint( display.BottomLeftReferencePoint )
-	-- grass.x, grass.y = 0, display.contentHeight
-	
-	-- -- define a shape that's slightly shorter than image bounds (set draw mode to "hybrid" or "debug" to see)
-	-- local grassShape = { -halfW,-34, halfW,-34, halfW,34, -halfW,34 }
-	-- physics.addBody( grass, "static", { friction=0.3, shape=grassShape } )
-	
-	-- -- all display objects must be inserted into group
-	-- group:insert( background )
-	-- group:insert( grass)
-	-- group:insert( crate )
+	local stick = StickLib.NewStick( 
+        {
+        x             = screenWidth*.1,
+        y             = screenHeight*.85,
+        thumbSize     = 16,
+        borderSize    = 32, 
+        snapBackSpeed = .75, 
+        R             = 255,
+        G             = 255,
+        B             = 255
+        } )
 
-	local background1 = display.newImage( "starreduniverse.jpg", display.contentWidth, display.contentHeight )
+
+	-- Moving starfield background
+	local background1 = display.newImage( "space.png", screenWidth, screenHeight )
 	background1:setReferencePoint( display.TopLeftReferencePoint )
 	background1.x, background1.y = 0, 0
-
-	local background2 = display.newImage( "starreduniverse.jpg", display.contentWidth, display.contentHeight )
+	local background2 = display.newImage( "space.png", screenWidth, screenHeight )
 	background2:setReferencePoint( display.TopLeftReferencePoint )
-	background2.x, background2.y = display.contentWidth, 0
+	background2.x, background2.y = background1.width, 0
+	local background3 = display.newImage( "space.png", screenWidth, screenHeight )
+	background3:setReferencePoint( display.TopLeftReferencePoint )
+	background3.x, background3.y = background1.width + background2.width, 0
 
-	function updateBackgroud()
-		background1.x = background1.x - speed
-		background2.x = background2.x - speed
+	local function updateBackgroud()
+		background1.x = background1.x - backgroundSpeed
+		background2.x = background2.x - backgroundSpeed
+		background3.x = background3.x - backgroundSpeed
 
-		if background1.x <= (-display.contentWidth) then
-			background1.x = display.contentWidth
+		if background1.x <= (-background1.width) then
+			background1.x = (background3.x +(background3.width))
 		end
 
-		if background2.x <= (-display.contentWidth) then
-			background2.x = display.contentWidth
+		if background2.x <= (-background2.width) then
+			background2.x = (background1.x + (background1.width))
+		end
+
+		if background3.x <= (-background3.width) then
+			background3.x = (background2.x + (background2.width))
+		end
+	end
+	
+	Runtime:addEventListener('enterFrame', updateBackgroud)
+
+
+	-- Setting the warrior on screen
+	local warrior = display.newImage( "starship.png")
+	warrior:setReferencePoint( display.TopLeftReferencePoint )
+	local warriorScaledHeight = warrior.height * warrior.yScale
+	local warriorScaledWidth = warrior.width * warrior.xScale
+	warrior.x, warrior.y = 20, (screenCenterY - (warriorScaledHeight/2))
+	physics.addBody(warrior, "static")
+
+	local xUpperBoundary = (screenRight - warriorScaledWidth)
+	local yUpperBoundary = (screenBottom - warriorScaledHeight)
+
+
+	-- Move our warrior by using the stick
+	local function moveWarrior(event)
+		if warrior.x <= screenLeft then warrior.x = screenLeft end
+		if warrior.x >= xUpperBoundary then warrior.x = xUpperBoundary end
+		if warrior.y <= screenTop then warrior.y = screenTop end
+		if warrior.y >= yUpperBoundary then warrior.y = yUpperBoundary end
+
+			stick:move(warrior, 7.0, false)
+
+			--warrior:applyForce(4000, 4000, warrior.x, warrior.y)
+	end
+
+	local function onCollision(event)
+		--
+	end
+
+	Runtime:addEventListener( "collision", onCollision )
+	Runtime:addEventListener('enterFrame', moveWarrior)
+
+	asteroids = {}
+	-- Generate random asteroid
+	local function generateAsteroid(event)
+		probability = 0.05
+
+		if math.random() < probability then
+			local asteroid = display.newImage( "asteroid".. (math.random(1, 4)) ..".png" )
+
+			physics.addBody(asteroid)
+			asteroid:setReferencePoint( display.TopLeftReferencePoint )
+			asteroid.x = screenRight
+			asteroid.y = (math.random() * (screenBottom - (asteroid.width * asteroid.xScale)))
+			asteroid.isFixedRotation = true
+			asteroids[#asteroids+1] = asteroid
+	
+			group:insert( asteroid )
+
+			local force = math.random()*10
+			asteroid:applyForce(force, (math.random(-1,1)*force/2), asteroid.x, asteroid.y)
+			asteroid:applyTorque( 100 )
+
+			print("Asteroid #" .. #asteroids)
 		end
 	end
 
-	Runtime:addEventListener('enterFrame', updateBackgroud)
-	local warrior = display.newImage( "warrior.png")
-	warrior:setReferencePoint( display.TopLeftReferencePoint )
+	local function moveAsteroids(event)
+		for index, asteroid in pairs(asteroids) do
+			asteroid.x = asteroid.x - backgroundSpeed
+			if asteroid.x == -(asteroid.width * asteroid.xScale) then
+				asteroids[index] = nil
+			end
+		end
+	end
 
-	warrior:scale(0.2, 0.2)
-	warrior.x, warrior.y = 20, ((display.contentHeight/2) - ((warrior.height*warrior.yScale)/2))
+	Runtime:addEventListener('enterFrame', generateAsteroid)
+	Runtime:addEventListener('enterFrame', moveAsteroids)
 
+	local shots = {}
+	local function onShoot(self, event)
+		local shot = display.newImage( "projectile2.png" )
+		shot.x = (warrior.x + warrior.width)
+		shot.y = (warrior.y + (warrior.height/2))
+		physics.addBody(shot, "static")
+		shots[#shots+1] = shot
+
+		group:insert(shot)
+	end
+
+	stick.Shoot:addEventListener("touch", onShoot)
+
+
+	local function moveShots(event)
+		for index, shot in pairs(shots) do
+			shot.x = shot.x + 5
+		end
+	end
+
+	Runtime:addEventListener('enterFrame', moveShots)
+
+
+
+	-- Insert elements into screen
 	group:insert( background1 )
 	group:insert( background2 )
+	group:insert( background3 )
 	group:insert( warrior )
+
 end
 
 -- Called immediately after scene has moved onscreen:
